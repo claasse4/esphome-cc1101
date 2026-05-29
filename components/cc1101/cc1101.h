2d980b0bf9a3eb007def53d4d8134dc9214c0c31
claasse4 <93863106+claasse4@users.noreply.github.com>
2026-05-29 15:23:54 +0200

    Refactor CC1101Component by removing unused code
    Removed unnecessary member variables and constructor from CC1101Component class.

--- components/cc1101/cc1101.h
+++ components/cc1101/cc1101.h
@@ -1,27 +1,13 @@
 #pragma once
-#include "esphome.h"
-#include <SPI.h>
+#include "esphome/core/component.h"
 
 namespace esphome {
 namespace cc1101 {
 
 class CC1101Component : public Component {
  public:
-  CC1101Component(GPIOPin *cs_pin, GPIOPin *gdo0_pin)
-      : cs_pin_(cs_pin), gdo0_pin_(gdo0_pin) {}
-
   void setup() override;
   void loop() override;
-
- protected:
-  void write_reg(uint8_t addr, uint8_t value);
-  void strobe(uint8_t cmd);
-  void init_radio();
-
-  GPIOPin *cs_pin_;
-  GPIOPin *gdo0_pin_;
-  SPIClass *spi_ = nullptr;
-  bool ready_ = false;
 };
 
 }  // namespace cc1101
