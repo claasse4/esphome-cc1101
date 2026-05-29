786b3e85805126e59345e4dfdcc81678f1b901ce
claasse4 <93863106+claasse4@users.noreply.github.com>
2026-05-29 15:26:44 +0200

    Update cc1101.h

--- components/cc1101/cc1101.h
+++ components/cc1101/cc1101.h
@@ -1,13 +1,21 @@
 #pragma once
 #include "esphome/core/component.h"
+#include "esphome/core/hal.h"
 
 namespace esphome {
 namespace cc1101 {
 
 class CC1101Component : public Component {
  public:
+  void set_cs_pin(GPIOPin *pin) { cs_pin_ = pin; }
+  void set_gdo0_pin(GPIOPin *pin) { gdo0_pin_ = pin; }
+
   void setup() override;
   void loop() override;
+
+ protected:
+  GPIOPin *cs_pin_{nullptr};
+  GPIOPin *gdo0_pin_{nullptr};
 };
 
 }  // namespace cc1101
