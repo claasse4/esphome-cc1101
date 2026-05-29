1b09a75c5900158de1e9d21d4f2f2b9d06b6b181
claasse4 <93863106+claasse4@users.noreply.github.com>
2026-05-29 15:20:53 +0200

    Implement CC1101Component with config schema
    Added CC1101Component and configuration schema for ESPHome.

--- components/cc1101/__init__.py
+++ components/cc1101/__init__.py
@@ -1,1 +1,13 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
 
+cc1101_ns = cg.esphome_ns.namespace("cc1101")
+CC1101Component = cc1101_ns.class_("CC1101Component", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(CC1101Component),
+})
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[cv.GenerateID()])
+    await cg.register_component(var, config)
