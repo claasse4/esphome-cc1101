9879f9595fa1f37e76c71c5fa15da0da2b022472
claasse4 <93863106+claasse4@users.noreply.github.com>
2026-05-29 15:27:09 +0200

    Update cc1101.cpp

--- components/cc1101/cc1101.cpp
+++ components/cc1101/cc1101.cpp
@@ -8,6 +8,8 @@ static const char *const TAG = "cc1101";
 
 void CC1101Component::setup() {
   ESP_LOGI(TAG, "CC1101 setup()");
+  if (cs_pin_) cs_pin_->setup();
+  if (gdo0_pin_) gdo0_pin_->setup();
 }
 
 void CC1101Component::loop() {
