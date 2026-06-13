e9d937612756ebbf39d38536ca87c3fcdb828ba1
claasse4 <93863106+claasse4@users.noreply.github.com>
2026-06-13 15:00:39 +0200

    Refactor cc1101_custom.h with new includes and methods
    Updated includes and added dump_config method.

--- components/cc1101_custom/cc1101_custom.h
+++ components/cc1101_custom/cc1101_custom.h
@@ -1,5 +1,12 @@
 #pragma once
-#include "esphome.h"
+
+#include "esphome/core/component.h"
+#include "esphome/core/hal.h"
+
+extern "C" {
+  #include "driver/spi_master.h"
+}
+
 #include "registers.h"
 
 namespace esphome {
@@ -11,19 +18,17 @@ class CC1101CustomComponent : public Component {
   void set_gdo0_pin(GPIOPin *pin) { gdo0_pin_ = pin; }
 
   void setup() override;
-  void loop() override;
+  void dump_config() override;
 
  protected:
-  GPIOPin *cs_pin_{nullptr};
-  GPIOPin *gdo0_pin_{nullptr};
-  spi_device_handle_t spi_{nullptr};
-  volatile bool pulse_flag_{false};
-
-  static void IRAM_ATTR gdo0_isr(void *arg);
-
+  void strobe(uint8_t cmd);
   void write_reg(uint8_t reg, uint8_t value);
   uint8_t read_reg(uint8_t reg);
-  void strobe(uint8_t cmd);
+
+  GPIOPin *cs_pin_{nullptr};
+  GPIOPin *gdo0_pin_{nullptr};
+
+  spi_device_handle_t spi_{nullptr};
 };
 
 }  // namespace cc1101_custom
