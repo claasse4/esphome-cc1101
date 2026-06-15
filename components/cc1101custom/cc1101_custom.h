421c6406c0963d144ee6efe305aa940282c04cbb
claasse4 <93863106+claasse4@users.noreply.github.com>
2026-06-15 18:29:47 +0200

    Refactor CC1101Custom class and update pin handling
    Refactor CC1101Custom class to use integers for pin representation and simplify methods.

--- components/cc1101custom/cc1101_custom.h
+++ components/cc1101custom/cc1101_custom.h
@@ -1,20 +1,17 @@
 #pragma once
-#include "esphome.h"
-#include "driver/gpio.h"
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace cc1101custom {
 
 class CC1101Custom : public Component {
  public:
-  GPIOPin *cs_pin_;
-  GPIOPin *gdo2_pin_;
-
-  void set_cs_pin(GPIOPin *pin) { cs_pin_ = pin; }
-  void set_gdo2_pin(GPIOPin *pin) { gdo2_pin_ = pin; }
+  int cs_pin;
+  int gdo2_pin;
 
   void setup() override;
-  void loop() override;
-  void dump_config() override;
-
- protected:
-  volatile uint32_t last_edge_us_ = 0;
-  static void IRAM_ATTR gdo2_isr_handler(void *arg);
+  void loop() override {}
 };
+
+}  // namespace cc1101custom
+}  // namespace esphome
