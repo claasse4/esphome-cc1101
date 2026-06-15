c3a350185360c1413e0c955a81f745dcb605a917
claasse4 <93863106+claasse4@users.noreply.github.com>
2026-06-15 18:33:44 +0200

    Fix logging for CS and GDO2 pin variables

--- components/cc1101custom/cc1101custom.cpp
+++ components/cc1101custom/cc1101custom.cpp
@@ -8,8 +8,8 @@ static const char *const TAG = "cc1101custom";
 
 void CC1101Custom::setup() {
   ESP_LOGI(TAG, "CC1101 custom component initialized");
-  ESP_LOGI(TAG, "CS pin: %d", this->cs_pin);
-  ESP_LOGI(TAG, "GDO2 pin: %d", this->gdo2_pin);
+  ESP_LOGI(TAG, "CS pin: %d", this->cs_pin_);
+  ESP_LOGI(TAG, "GDO2 pin: %d", this->gdo2_pin_);
 }
 
 }  // namespace cc1101custom
