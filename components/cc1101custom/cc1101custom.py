013bf1d09e1c431c91ec779ad2b1de41f4fa8012
claasse4 <93863106+claasse4@users.noreply.github.com>
2026-06-15 23:15:29 +0200

    Refactor CONFIG_SCHEMA and variable initialization

--- components/cc1101custom/cc1101custom.py
+++ components/cc1101custom/cc1101custom.py
@@ -4,12 +4,21 @@ import esphome.config_validation as cv
 cc1101_ns = cg.esphome_ns.namespace("cc1101custom")
 CC1101Custom = cc1101_ns.class_("CC1101Custom", cg.Component)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(CC1101Custom),
-    cv.Required("cs_pin"): cv.int_,
-    cv.Required("gdo2_pin"): cv.int_,
-})
+CONF_CS_PIN = "cs_pin"
+CONF_GDO2_PIN = "gdo2_pin"
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(CC1101Custom),
+        cv.Required(CONF_CS_PIN): cv.int_,
+        cv.Required(CONF_GDO2_PIN): cv.int_,
+    }
+)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config["id"], config["cs_pin"], config["gdo2_pin"])
+    var = cg.new_Pvariable(
+        config[cv.CONF_ID],
+        config[CONF_CS_PIN],
+        config[CONF_GDO2_PIN],
+    )
     await cg.register_component(var, config)
