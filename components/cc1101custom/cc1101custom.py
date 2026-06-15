20fcf1ca28557a8252caef985210ea6b71e27c73
claasse4 <93863106+claasse4@users.noreply.github.com>
2026-06-15 22:09:25 +0200

    Add CC1101Custom component with configuration schema

--- components/cc1101custom/cc1101custom.py
+++ components/cc1101custom/cc1101custom.py
@@ -1,1 +1,15 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
 
+cc1101_ns = cg.esphome_ns.namespace("cc1101custom")
+CC1101Custom = cc1101_ns.class_("CC1101Custom", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(CC1101Custom),
+    cv.Required("cs_pin"): cv.int_,
+    cv.Required("gdo2_pin"): cv.int_,
+})
+
+async def to_code(config):
+    var = cg.new_Pvariable(config["id"], config["cs_pin"], config["gdo2_pin"])
+    await cg.register_component(var, config)
