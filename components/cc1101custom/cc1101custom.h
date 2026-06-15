15342e62249fef0a35464383bf8dc0fbb951beed
claasse4 <93863106+claasse4@users.noreply.github.com>
2026-06-15 18:33:17 +0200

    Refactor CC1101Custom class constructor
    Refactor constructor to initialize member variables.

--- components/cc1101custom/cc1101custom.h
+++ components/cc1101custom/cc1101custom.h
@@ -6,11 +6,15 @@ namespace cc1101custom {
 
 class CC1101Custom : public Component {
  public:
-  int cs_pin;
-  int gdo2_pin;
+  CC1101Custom(int cs_pin, int gdo2_pin)
+      : cs_pin_(cs_pin), gdo2_pin_(gdo2_pin) {}
 
   void setup() override;
   void loop() override {}
+
+ protected:
+  int cs_pin_;
+  int gdo2_pin_;
 };
 
 }  // namespace cc1101custom
