ac78707e833f4bae09ee37120ecc4ac0a6e75fe0
claasse4 <93863106+claasse4@users.noreply.github.com>
2026-06-15 23:16:35 +0200

    Remove logging header from cc1101custom.cpp
    Removed unnecessary logging header from cc1101custom.cpp.

--- components/cc1101custom/cc1101custom.cpp
+++ components/cc1101custom/cc1101custom.cpp
@@ -1,5 +1,4 @@
 #include "cc1101custom.h"
-#include "esphome/core/log.h"
 
 namespace esphome {
 namespace cc1101custom {
@@ -10,6 +9,7 @@ void CC1101Custom::setup() {
   ESP_LOGI(TAG, "CC1101 custom component initialized");
   ESP_LOGI(TAG, "CS pin: %d", this->cs_pin_);
   ESP_LOGI(TAG, "GDO2 pin: %d", this->gdo2_pin_);
+  // Hier straks je CC1101 init
 }
 
 }  // namespace cc1101custom
