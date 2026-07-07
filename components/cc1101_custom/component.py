b1966f92267bf18d9d893a456e3091e16b5b070c
claasse4 <93863106+claasse4@users.noreply.github.com>
2026-07-07 09:44:50 +0200

    Add CC1101Custom component with configuration schema

--- components/cc1101_custom/component.py
+++ components/cc1101_custom/component.py
@@ -1,1 +1,24 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import spi
+from esphome.const import CONF_ID
 
+cc1101_ns = cg.esphome_ns.namespace("cc1101_custom")
+CC1101Custom = cc1101_ns.class_("CC1101Custom", cg.Component, spi.SPIDevice)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(CC1101Custom),
+    cv.Required("spi_id"): cv.use_id(spi.SPIComponent),
+    cv.Required("cs_pin"): cv.pin,
+    cv.Required("gdo0_pin"): cv.pin,
+    cv.Required("frequency"): cv.string,
+    cv.Required("modulation_type"): cv.string,
+})
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield spi.register_spi_device(var, config["spi_id"])
+    cg.add(var.set_cs_pin(config["cs_pin"]))
+    cg.add(var.set_gdo0_pin(config["gdo0_pin"]))
+    cg.add(var.set_frequency(config["frequency"]))
+    cg.add(var.set_modulation_type(config["modulation_type"]))
