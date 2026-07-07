c3ddd261fe7d0886d90d5a2e260ba73346daacaf
claasse4 <93863106+claasse4@users.noreply.github.com>
2026-07-07 10:18:25 +0200

    Add automation action for cc1101.begin_rx

--- components/cc1101_custom/component.py
+++ components/cc1101_custom/component.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 import esphome.pins as pins
+import esphome.automation as automation
 from esphome.components import spi
 from esphome.const import CONF_ID
 
@@ -23,3 +24,19 @@ def to_code(config):
     cg.add(var.set_gdo0_pin(config["gdo0_pin"]))
     cg.add(var.set_frequency(config["frequency"]))
     cg.add(var.set_modulation_type(config["modulation_type"]))
+    return var
+
+# -----------------------------
+# ACTION: cc1101.begin_rx
+# -----------------------------
+
+@automation.register_action(
+    "cc1101.begin_rx",
+    automation.Action,
+    cv.Schema({cv.Required(CONF_ID): cv.use_id(CC1101Custom)})
+)
+def cc1101_begin_rx_to_code(config, action_id, template_arg):
+    var = cg.new_Pvariable(action_id)
+    cc1101 = cg.get_variable(config[CONF_ID])
+    cg.add(cc1101.begin_rx())
+    return var
